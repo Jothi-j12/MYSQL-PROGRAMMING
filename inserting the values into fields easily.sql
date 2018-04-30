5f70347720b8cb0aba2a0e009425bfafe202e2d1
Jothi.J <37336326+Jothi-j12@users.noreply.github.com>
2018-04-30 11:46:23 +0530

    Create inserting the values into fields easily.sql

---
+++ inserting the values into fields easily.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE student;
+use student;
+create table user(
+userid varchar(100),username varchar(100),age varchar(15),dept varchar(110),cgpa varchar(18),sgpa varchar(20),native varchar(100),percentage int(30),deptid varchar(16),rankno int(100));
+insert into user values("1","jothi","17","ece","7","7","covai","89","12","5"),("1","sri","17","ece","7","7","covai","89","12","5"),
+("1","sandy","17","ece","7","7","covai","89","12","5");
