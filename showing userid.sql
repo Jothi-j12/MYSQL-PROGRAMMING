92ef2ba366750e8db69c675895a644d6835c51d6
Jothi.J <37336326+Jothi-j12@users.noreply.github.com>
2018-04-30 11:07:30 +0530

    Create showing userid.sql

---
+++ showing userid.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE student;
+use student;
+create table user(
+userid varchar(100),username varchar(100),age varchar(15),dept varchar(110),cgpa varchar(18),sgpa varchar(20),native varchar(100),
+percentage int(30),deptid varchar(16),rankno int(100));
+insert into user values("1","j","17","ece","7","7","covai","89","12","5");
+select userid from user;
