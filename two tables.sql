9cd80d5a349f8159ac18d78a94431a3f8e0b1429
Jothi.J <37336326+Jothi-j12@users.noreply.github.com>
2018-05-01 11:12:46 +0530

    Create two tables.sql

---
+++ two tables.sql
@@ -0,0 +1,10 @@
+create database student;
+use student;
+create table details(rollno varchar(100),name char(19),dept char(188));
+insert into details values('1','jothi','ece');
+create table empdetails(empno varchar(100),employeename char(199));
+insert into empdetails values('12','sri');
+show databases;
+select * from details;
+select * from empdetails;
+
